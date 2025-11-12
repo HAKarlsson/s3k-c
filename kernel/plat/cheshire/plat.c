17a2fdee634f80ed52114def14bee3887e96bf3e
Clément Chavanon <clement.chavanon@inria.fr>
2025-11-12 18:02:27 +0100

    Fix release mode

--- kernel/plat/cheshire/plat.c
+++ kernel/plat/cheshire/plat.c
@@ -22,13 +22,13 @@ void kernel_init(void)
 	cap_mk_time(&init_caps[3], 0, S3K_SLOT_CNT);
 	cap_mk_monitor(&init_caps[4], 0, S3K_PROC_CNT);
 	cap_mk_channel(&init_caps[5], 0, S3K_CHAN_CNT);
-	for (int i = 0; i < ARRAY_SIZE(init_caps); ++i) {
 #ifndef NDEBUG
+	for (int i = 0; i < ARRAY_SIZE(init_caps); ++i) {
 		kprintf("# init_caps[%d]: ", i);
-#endif
 		cap_print(&init_caps[i]);
 		kprintf("\n");
 	}
+#endif
 	ctable_init(init_caps, ARRAY_SIZE(init_caps));
 
 	sched_init();
